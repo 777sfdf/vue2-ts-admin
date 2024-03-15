6abf813748255e041facd21322506ba902e9aeac
777sfdf1 <2075594646@qq.com>
2024-03-15 09:11:57 +0800

    第一次测试提交

---
+++ src/views/layout/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>布局测试页面</h2>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>登录测试页面</h1>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style></style>
